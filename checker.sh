e37cc89e1bb8c828f7e45fcd5d877d2e4995293c
Ryan W <w@lker.io>
2021-02-19 14:41:17 -0500

    initial commit

---
+++ checker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+pname=`basename $0 .sh`
+
+walgreensapi='https://www.walgreens.com/hcschedulersvc/svc/v1/immunizationLocations/availability'
+currentdate=`date --rfc-3339=date`
+coordinates='37.7030051 -85.8647201
+36.972813 -86.4549821'
+
+echo "$coordinates" |
+	while read line; do
+		if [ -z "$line" ]; then
+			continue
+		fi
+
+		tmpfile=$(mktemp /tmp/$pname.XXXXXX)
+
+		lat=`echo $line | cut -d' ' -f1`
+		long=`echo $line | cut -d' ' -f2`
+
+		payload='{ "serviceId": "99", "position": { "latitude": '$lat', "longitude": '$long' }, "appointmentAvailability":{"startDateTime": "'$currentdate'"},"radius": 25}'
+
+		curl -sS -X POST $walgreensapi -H 'Content-Type: application/json' -d "$payload" -o $tmpfile
+		cat "$tmpfile"|json_pp	
+		rm "$tmpfile"
+	done
+
